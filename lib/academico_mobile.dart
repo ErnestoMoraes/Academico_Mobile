339e17b40a75d50b44316db08371615a25affdc5
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-03-11 16:42:51 -0300

    Instalando Images

---
+++ lib/academico_mobile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AcademicoMobile extends StatelessWidget {
+  const AcademicoMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Academico Mobile',
+      routes: {
+        '/': (context) => Container(),
+      },
+    );
+  }
+}
