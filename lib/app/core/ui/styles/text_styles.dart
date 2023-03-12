2311e5bff3f2d97fa2b85baad3caa7c0b1ceb7e4
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-03-12 09:32:52 -0300

    Adicionando fonts de labes diferentes

--- lib/app/core/ui/styles/text_styles.dart
+++ lib/app/core/ui/styles/text_styles.dart
@@ -28,6 +28,17 @@ class TextStyles {
 
   TextStyle get textButtonLabel =>
       textBold.copyWith(color: ColorsApp.instance.labelblack1, fontSize: 20);
+
+  TextStyle get texLabelH1 =>
+      textBold.copyWith(color: ColorsApp.instance.cardwhite, fontSize: 32);
+  TextStyle get texLabelH2 =>
+      textRegular.copyWith(color: ColorsApp.instance.cardwhite, fontSize: 20);
+  TextStyle get texLabelH3 =>
+      textRegular.copyWith(color: ColorsApp.instance.cardwhite, fontSize: 18);
+  TextStyle get texLabelH4 =>
+      textRegular.copyWith(color: ColorsApp.instance.cardwhite, fontSize: 16);
+  TextStyle get texLabelH5 =>
+      textRegular.copyWith(color: ColorsApp.instance.cardwhite, fontSize: 12);
 }
 
 extension TextStylesExtension on BuildContext {
